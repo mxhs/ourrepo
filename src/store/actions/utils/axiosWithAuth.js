4e28ad1ff3ba65373e3af660ab46bfc4c2899497
Pablo Vahanian <51976194+pvahanian@users.noreply.github.com>
2020-10-22 15:46:37 -0700

    Update axiosWithAuth.js

--- src/store/actions/utils/axiosWithAuth.js
+++ src/store/actions/utils/axiosWithAuth.js
@@ -3,8 +3,6 @@ import axios from "axios";
 // get the token from localstorage
 export const axiosWithAuth = () => {
   let token = window.localStorage.getItem("token");
-  token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhYmxvNDQ3IiwidXNlcklkIjo2MywiaWF0IjoxNjAzMzgwMjAxLCJleHAiOjE2MDMzOTgyMDF9.R86x328NiiB5eDq9Z9pT9xXhMY0Y-B-bD9gXcR9vu4U";
   return axios.create({
     headers: {
       authorization: token
