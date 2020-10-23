a4863038a7ca03409915d10989266419fd4e8fa7
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-10-22 18:05:28 -0700

    plz ziggs gods help

--- src/store/actions/utils/axiosWithAuth.js
+++ src/store/actions/utils/axiosWithAuth.js
@@ -3,13 +3,11 @@ import axios from "axios";
 // get the token from localstorage
 export const axiosWithAuth = () => {
   let token = window.localStorage.getItem("token");
-  token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhYmxvNDQ3IiwidXNlcklkIjo2MywiaWF0IjoxNjAzMzgwMjAxLCJleHAiOjE2MDMzOTgyMDF9.R86x328NiiB5eDq9Z9pT9xXhMY0Y-B-bD9gXcR9vu4U";
+  console.log(token)
   return axios.create({
     headers: {
       authorization: token
     }
-    // baseURL: "https://pintereach-backend-ajg.herokuapp.com/"
   });
 };
 
